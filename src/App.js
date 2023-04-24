91007850d1dab322c822e3c89e690b2ba89e5288
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2023-04-24 20:57:52 +0200

    Add select button with state

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ import { string } from 'prop-types';
 import { number } from 'prop-types';
 import React from 'react';
 
-const VerseRow = ({ verse }) => (
+const VerseRow = ({ verse, onSelect }) => (
   <tr>
     <td>{verse.book_name}</td>
     <td>{verse.chapter + ":" + verse.verse}</td>
     <td>{verse.text}</td>
+    <td>
+      <button
+        onClick={() => onSelect(verse)}
+      >Select</button>
+    </td>
   </tr>
 )
 
@@ -23,35 +28,55 @@ VerseRow.propTypes = {
     verse: number,
     text: string
   }),
+  onSelect: PropTypes.func,
 }
 
 function App() {
   const [filter, setFilter] = React.useState('');
+  const [selectedItem, setSelectedItem] = React.useState(null);
 
   return (
     <div style={{ margin: 'auto', width: 800, paddingTop: '1rem'}}>
       <h1 className='title'>The Bible: TL;DR</h1>
-      <input
-        value={filter}
-        onChange={(e) => setFilter(e.target.value)} />
-      <table width="100%">
-        <thead>
-          <tr>
-            <th>Book</th>
-            <th>Verse</th>
-            <th>Text</th>
-          </tr>
-        </thead>
-        <tbody>
-          {bible
-            .filter((verse) => verse.text.toLowerCase().includes(filter.toLowerCase()))
-            .slice(0, 20)
-            .map((verse) => (
-              <VerseRow verse={verse} key={verse.book_id + verse.chapter + verse.verse} />
-            ))
-          }
-        </tbody>
-      </table>
+      <div
+        style={{
+          display: 'grid',
+          gridColumnGap: '1rem',
+        }}
+      >
+        <div>
+          <input
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)} />
+          <table width="100%">
+            <thead>
+              <tr>
+                <th>Book</th>
+                <th>Verse</th>
+                <th>Text</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bible
+                .filter((verse) => verse.text.toLowerCase().includes(filter.toLowerCase()))
+                .slice(0, 20)
+                .map((verse) => (
+                  <VerseRow
+                    key={verse.book_id + verse.chapter + verse.verse}
+                    onSelect={() => setSelectedItem(verse)}
+                    verse={verse}
+                  />
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
+        {selectedItem && (
+          <div>
+            <h1>{selectedItem.book_name + " " + selectedItem.chapter + ":" + selectedItem.verse}</h1>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
