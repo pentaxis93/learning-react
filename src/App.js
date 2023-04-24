1ffa96576d2939406b5efa8e421d63533e063d15
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2023-04-24 18:22:21 +0200

    Add case-insensitve search bar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import bible from './asv.json';
 import PropTypes from 'prop-types'
 import { string } from 'prop-types';
 import { number } from 'prop-types';
+import React from 'react';
 
 const VerseRow = ({ verse }) => (
   <tr>
@@ -25,15 +26,14 @@ VerseRow.propTypes = {
 }
 
 function App() {
+  const [filter, setFilter] = React.useState('');
+
   return (
-    <div
-      style={{
-        margin: 'auto',
-        width: 800,
-        paddingTop: '1rem',
-      }}
-    >
+    <div style={{ margin: 'auto', width: 800, paddingTop: '1rem'}}>
       <h1 className='title'>The Bible: TL;DR</h1>
+      <input
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)} />
       <table width="100%">
         <thead>
           <tr>
@@ -43,9 +43,13 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {bible.slice(0, 20).map((verse) => (
-            <VerseRow verse={verse} key={verse.book_id + verse.chapter + verse.verse} />
-          ))}
+          {bible
+            .filter((verse) => verse.text.toLowerCase().includes(filter.toLowerCase()))
+            .slice(0, 20)
+            .map((verse) => (
+              <VerseRow verse={verse} key={verse.book_id + verse.chapter + verse.verse} />
+            ))
+          }
         </tbody>
       </table>
     </div>
